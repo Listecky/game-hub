ea033ee4cef4516623488c8aacb0e05d9ab147e7
Listecky <listwan13@gmail.com>
2023-11-05 20:46:09 +0100

    implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,8 @@ const useGames = (gameQuery: GameQuery) => useData<Game>("/games",
   params: {
     genres: gameQuery.genre?.id, 
     platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder
+    ordering: gameQuery.sortOrder,
+    search: gameQuery.searchText
 }
 },
  [gameQuery]); //Array of dependencies, when to render the object
